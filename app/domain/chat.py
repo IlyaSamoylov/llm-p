efc23debc4c152e0efc8613d9a8407438f3f368b
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-04-15 19:40:43 +0500

    fix temp in-memory storage to global

--- app/domain/chat.py
+++ app/domain/chat.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Literal
+from datetime import datetime
 
 _ALLOWED_ROLES = {"system", "user", "assistant"}
 
@@ -7,6 +8,18 @@ _ALLOWED_ROLES = {"system", "user", "assistant"}
 class ChatMessage:
 	role: Literal["system", "user", "assistant"]
 	content: str
+
+	def __post_init__(self):
+		if self.role not in _ALLOWED_ROLES:
+			raise ValueError(f"Invalid role: {self.role}")
+
+@dataclass
+class BDMessage:
+	id: int
+	user_id: int
+	role: Literal["system", "user", "assistant"]
+	content: str
+	created_at: datetime
 
 	def __post_init__(self):
 		if self.role not in _ALLOWED_ROLES:
--- app/repositories/chat_messages.py
+++ app/repositories/chat_messages.py
@@ -1,19 +1,19 @@
 from app.domain.chat import ChatMessage, BDMessage
-from datetime import datetime
+from datetime import datetime, timezone
+
+_STORAGE: list[BDMessage] = []
+_ID_SEQ: int = 1
+
 
 class MsgsRepo:
-	def __init__(self):
-		self._msgs: list[BDMessage] = []
-		self._id_seq: int = 1
-
 	async def get_history(self, user_id: int, max_history: int = -1) -> list[ChatMessage]:
 
-		user_msgs: list[BDMessage] = [msg for msg in self._msgs if msg.user_id == user_id]
-
+		user_msgs: list[BDMessage] = [msg for msg in _STORAGE if msg.user_id == user_id]
 		user_msgs.sort(key=lambda m: m.created_at)
+
 		if max_history == -1:
 			selected = user_msgs
-		if max_history == 0:
+		elif max_history == 0:
 			selected = []
 		else:
 			selected = user_msgs[-max_history:]
@@ -21,10 +21,13 @@ class MsgsRepo:
 		return [ChatMessage(role=m.role, content=m.content) for m in selected]
 
 	async def add_msg(self, user_id: int, message: ChatMessage):
-		db_msg = BDMessage(id=self._id_seq, user_id=user_id, role=message.role,
-                            content=message.content, created_at=datetime.now())
-		self._id_seq += 1
-		self._msgs.append(db_msg)
+		global _ID_SEQ
+
+		db_msg = BDMessage(id=_ID_SEQ, user_id=user_id, role=message.role,
+                            content=message.content, created_at=datetime.now(timezone.utc))
+		_ID_SEQ += 1
+		_STORAGE.append(db_msg)
 
 	async def delete_history(self, user_id: int):
-		self._msgs = [msg for msg in self._msgs if msg.user_id != user_id]
+		global _STORAGE
+		_STORAGE = [msg for msg in _STORAGE if msg.user_id != user_id]
