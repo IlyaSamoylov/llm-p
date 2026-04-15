cd2f9e1da2a5b4547c98db1f9964f4f4c43f67ef
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-04-15 19:45:39 +0500

    add route-level for chat

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,9 +1,22 @@
-import httpx
+from fastapi import Depends, Request
 
-async def get_httpx_client():
-	"""Функция создания асинхронного клиента"""
-	async with httpx.AsyncClient(
-			timeout=httpx.Timeout(60.0, connect=5.0),
-			limits=httpx.Limits(max_keepalive_connections=5)
-	) as client:
-		yield client
+from app.usecases.chat import ChatUsecases
+from app.repositories.chat_messages import MsgsRepo
+from app.services.openrouter_client import OpenRouterClient
+
+async def get_current_uid():
+	return 1
+
+
+async def get_http_client(request: Request):
+	return request.app.state.http_client
+
+async def get_openrouter_client(http_client = Depends(get_http_client)) -> OpenRouterClient:
+	return OpenRouterClient(client=http_client)
+
+async def get_repo() -> MsgsRepo:
+	return MsgsRepo()
+
+async def get_chat_usecase(repo: MsgsRepo = Depends(get_repo),
+                           client: OpenRouterClient = Depends(get_openrouter_client)) -> ChatUsecases:
+	return ChatUsecases(repo, client)
