da20bfb493247a3ddac4909111256785b900ce39
IlyaSamoylov <ilya.samoylov2004@mail.ru>
2026-04-17 01:48:16 +0500

    Add table creation on startup and get_session in dependencies

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,12 +1,18 @@
 from fastapi import Depends, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import AsyncGenerator
 
 from app.usecases.chat import ChatUsecases
 from app.repositories.chat_messages import MsgsRepo
 from app.services.openrouter_client import OpenRouterClient
+from app.db.session import AsyncSessionLocal
 
 async def get_current_uid():
 	return 1
 
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+	async with AsyncSessionLocal() as session:
+		yield session
 
 async def get_http_client(request: Request):
 	return request.app.state.http_client
@@ -14,8 +20,8 @@ async def get_http_client(request: Request):
 async def get_openrouter_client(http_client = Depends(get_http_client)) -> OpenRouterClient:
 	return OpenRouterClient(client=http_client)
 
-async def get_repo() -> MsgsRepo:
-	return MsgsRepo()
+async def get_repo(session: AsyncSession = Depends(get_session)) -> MsgsRepo:
+	return MsgsRepo(session)
 
 async def get_chat_usecase(repo: MsgsRepo = Depends(get_repo),
                            client: OpenRouterClient = Depends(get_openrouter_client)) -> ChatUsecases:
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import httpx
 
+from app.db.base import Base
+from app.db.session import engine
 from app.api.routes_chat import chat_router
 from app.core.config import settings
 
@@ -11,11 +13,16 @@ async def lifespan(app: FastAPI):
         timeout=httpx.Timeout(60.0, connect=5.0),
         limits=httpx.Limits(max_keepalive_connections=5)
     ) as client:
+        # сохранение единого для приложения асинхронного клиента
         app.state.http_client = client
+
+        # создание таблиц бд
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
         yield
 
 def create_app() -> FastAPI:
-
     app = FastAPI(lifespan=lifespan, title=settings.APP_NAME)
     app.include_router(chat_router)
 
